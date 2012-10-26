7fa95f38bc9b2180ad4e0c8e25adde2db1a0bde8
Luke Kavanagh <ltkavanagh@gmail.com>
2012-10-26 11:56:37 +0100

    Adding: cleanup tmp dir on SIGTERM; option to set graphics engine to Imagemagick (as Heroku only has imagemagick available); changed they way temp files are named - base64 was a ridiculous idea.

--- app.js
+++ app.js
@@ -9,56 +9,38 @@ var express = require('express'),
 
 program
   .version('0.0.1')
-  .option('-p, --port <n>', 'Set the port number', parseInt, process.env.PORT || 3000)
+  .option('-p, --port <n>', 'Set the port number', parseInt, process.env.PORT || 3000 )
+  .option('-i --imagemagick', 'Use Imagemagick instead of Graphicsmagick', process.env.IMAGEMAGICK || false )
   .parse(process.argv);
 
-var startTime = Date.now(),
+var engine = program.imagemagick ? gm.subClass({ imageMagick: true }) : gm,
+	startTime = Date.now(),
 	tmpDir = 'tmp/' + program.port + '-' + startTime,
 	imagesToDelete = [];
 
 try{
 	fs.mkdirSync( 'tmp', 0755 );
 }catch(e){
-
+	process.stderr.write( 'Error creating top-level tmp directory\n' );
 }
 
 try{
 	fs.mkdirSync( tmpDir, 0755 );
+	process.on('exit', cleanUpOnShutDown);
+	process.on('SIGTERM', cleanUpOnShutDown);
 }catch(e){
-
+	process.stderr.write( 'Error creating app instance tmp directory\n' );
 }
-
-function deleteFile( filename ) {
-	fs.unlink( filename, function ( err ) {
-		if ( err ) {
-			process.stderr.write( 'Error deleting temporary image ' + filename + '\n' );
-			process.stderr.write( err );
-			return;
-		}
-		
-		process.stdout.write('Deleted temporary image ' + filename + '\n');
-	});
-}
-
-setInterval(function(){
-
-	var queue = imagesToDelete;
-
-	imagesToDelete = [];
-
-	queue.forEach( deleteFile );
-
-}, 30000);
 
 app.get('/:width/:height', function(req, res){
 
 	var params = req.params,
 		query = req.query,
-		tmpFileName = [tmpDir,'/', new Buffer(req.query.url).toString('base64'), '.jpg'].join( '' );
+		tmpFileName = [tmpDir,'/', getRandomFileName(), '.jpg'].join( '' );
 
 	request( req.query.url ).pipe(fs.createWriteStream(tmpFileName).on('close', function(){
 
-		var img = gm(fs.createReadStream(tmpFileName));
+		var img = engine(fs.createReadStream(tmpFileName));
 
 		if ( query.crop ) {
 			var cropVals = query.crop.split(',');
@@ -73,8 +55,37 @@ app.get('/:width/:height', function(req, res){
 			stdout.pipe(res);
 		});
 	}));
-
 });
+
+function getRandomFileName() {
+	return Math.floor(Math.random(100) * 10000).toString() + Date.now().toString();
+}
+
+function deleteFile( filename ) {
+	fs.unlink( filename, function ( err ) {
+		if ( err ) {
+			process.stderr.write( 'Error deleting temporary image ' + filename + '\n' );
+			console.log(err);
+			return;
+		}
+		
+		process.stdout.write('Deleted temporary image ' + filename + '\n');
+	});
+}
+
+function cleanUpOnShutDown(){
+	fs.rmdirSync( tmpDir );
+}
+
+setInterval(function(){
+
+	var queue = imagesToDelete;
+
+	imagesToDelete = [];
+
+	queue.forEach( deleteFile );
+
+}, 30000);
 
 app.enable('trust proxy');
 app.listen( program.port );
